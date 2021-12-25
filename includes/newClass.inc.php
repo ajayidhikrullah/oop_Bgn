246812c929755fb41f21adf1877c8ea9cf659d17
ajayidhikrullah <ajayidhikrullah@gmail.com>
2021-12-25 02:36:15 +0000

    Visibility and inheritance i OOP, created class, Object and properties

---
+++ includes/newClass.inc.php
@@ -0,0 +1,13 @@
+<?php
+class NewClass {
+    // properties goes in here + Methods
+    public $info = 'Some Informations goes here...';
+
+}
+
+//this is how to instantiate a class i.e: call a class
+
+$instantiateClass = new NewClass;
+var_dump($instantiateClass);
+
+
