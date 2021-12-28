2bd1073b96183afbef3fc005a32caad8e703fcec
ajayidhikrullah <ajayidhikrullah@gmail.com>
2021-12-28 05:15:31 +0000

    created the Autoloader such that classes are executed automatically

--- calculator_oop/includes/calc.inc.php
+++ calculator_oop/includes/calc.inc.php
@@ -1,3 +1,16 @@
 <?php
-
+        declare(strict_types = 1);
+        include 'class-autoload.inc.php';
+    
+        $op = $_POST['operator'];
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+    
+        $calc = new Calc((string)$op, (int)$num1, (int)$num2);
+    
+        try {
+            echo $calc->calculator();
+        } catch (TypeError $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
 ?>
