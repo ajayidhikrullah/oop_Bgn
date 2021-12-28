669e3fe728d075e0624f09f8813045b4d8ae8209
ajayidhikrullah <ajayidhikrullah@gmail.com>
2021-12-28 01:04:13 +0000

    created the forms page with number input

--- calculator_oop/indexCalc.php
+++ calculator_oop/indexCalc.php
@@ -14,6 +14,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="includes/calc.inc.php" method="post">
+        <h3>OOP calculator</h3>
+            <input type="number" name="num1" placeholder = "input First number to calculate">
+            <select name="oper">
+                <option value="add">Addition</option>
+                <option value="sub">Subtraction</option>
+                <option value="div">Division</option>
+                <option value="mul">Multiplication</option>
+            </select>
+
+            <input type="number" name="num2" placeholder="second number to calculate">
+            <button type="submit" name = "submit">Calculate</button>
+    </form>
+
 </body>
 </html>
