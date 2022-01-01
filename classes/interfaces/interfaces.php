3adacf0323c2e276f594cc20bba129c9181d9dc4
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-01-01 02:57:33 +0000

    created the interface and implements function to structure and create rules for the classes, methods and properties, instantiation

--- classes/interfaces/interfaces.php
+++ classes/interfaces/interfaces.php
@@ -1,5 +1,47 @@
 <?php
-    
+
+interface PaymentInterface {
+    public function PayNow();
+}
+    class Paypal implements PaymentInterface, LoginInterface {
+        public function loginFirst(){};
+        public function payNow(){}
+        public function paymentProcess(){
+            $this->loginFirst();
+            $this->payNow();
+        }
+    }
+
+    class Visa implements PaymentInterface, LoginInterface{
+        public function payNow(){}
+        public function paymentProcess(){
+            $this->payNow();
+        }
+    }
+
+    class Cash implements PaymentInterface{
+        public function payNow(){}
+        public function paymentProcess(){
+            $this->payNow();
+        }
+    }
+
+
+    //buy product class using the above method and classes
+    class BuyProduct {
+        public function pay(PaymentInterface $paymentType){
+            $paymentType->paymentProcess();
+        }
+
+        public function onlinePay(loginInterface $paymentType){
+            $paymentType->paymentProcess();
+        }
+    }
+
+
+    $paymentType = new Cash();
+    $buyProduct = new BuyProduct();
+    $buyProduct->pay($paymentType);
 
 
 
