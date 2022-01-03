c7864c944032316a4a5c192327bd4d9b17c94b70
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-01-03 01:22:39 +0000

    Created the database handler class

---
+++ OOP_PROJECT/classes/dbh.classes.php
@@ -0,0 +1,14 @@
+<?php
+
+    class Dbh {
+        private function connect(){
+            try {
+                $username = 'root';
+                $password = '';
+                $dbh = new PDO('mysqli:host=localhost;dbname=?', $username, $password);
+                return $dbh;
+            } catch (PDOEXception $e) {
+                print "Oops, " . $e->getMessage() . "<br>";
+            }
+        }
+    }
