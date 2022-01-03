c99c407d664425d47a71598fb4fed5e4be71a5fa
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-01-03 00:57:04 +0000

    Initiated the final OOP login and signup project using Pure Php OOP, Classes, methods and properties were used all through, Instantiation and inheritance were also applied, extending to another class to puse its properties was also used to achieve this project

---
+++ OOP_PROJECT/includes/signup.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    // grab the data from users in frontend
+    $uid = $_POST['uid'];
+    $pwd = $_POST['pwd'];
+    $pwdRepeat = $_POST['pwdrepeat'];
+    $email = $_POST['email'];
+
+    //instantiate SignUpController class goes
+
+
+
+    //Run error handlers and user signup
+
+
+    //going back to landing page or front page
+}
